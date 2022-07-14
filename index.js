2e3d859858e1b9e81fbca030e8b91b2ceb48d819
sazidahmednassir <sazidahmednassir@gmail.com>
2022-07-14 02:33:03 -0800

    add connection in db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
@@ -7,6 +8,9 @@ require('dotenv').config();
 
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3lazc.mongodb.net/?retryWrites=true&w=majority`;
+
 
 app.get('/', (req, res) => {
   res.send('Hello World from server!')
